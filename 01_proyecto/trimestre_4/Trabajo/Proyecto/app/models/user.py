9f66e5b2adcc57e6169d7376071b23155d921450
PaulaC <paulaacassiani@gmail.com>
2025-12-16 11:51:46 -0500

    Add docstring to User model

--- 01_proyecto/trimestre_4/Trabajo/Proyecto/app/models/user.py
+++ 01_proyecto/trimestre_4/Trabajo/Proyecto/app/models/user.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model, UserMixin):
+    """User model for authentication and user management."""
     __tablename__ = 'usuarios'
 
     usuario_id = db.Column(db.Integer, primary_key=True)
