33f8381b8c04e2217139edeb614a1408a37ea86b
PaulaC <paulaacassiani@gmail.com>
2025-12-16 11:42:00 -0500

    Add initialization comment in app.py

--- 01_proyecto/trimestre_4/Trabajo/Proyecto/app.py
+++ 01_proyecto/trimestre_4/Trabajo/Proyecto/app.py
@@ -1,7 +1,9 @@
 # Main application entry point for RestauSync Flask app
+# This initializes the Flask app
 from app import create_app
 
 app = create_app()
 
+# Run the application in debug mode when executed directly
 if __name__ == '__main__':
     app.run(debug=True)
